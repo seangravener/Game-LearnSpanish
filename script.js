4cc45eb35f79a6231c6e46b622c7968247b396ec
Sean Gravener <sean@gravener.net>
2022-06-01 08:02:35 -0400

    Organize named funcs into object methods

--- script.js
+++ script.js
@@ -4,31 +4,33 @@ let question = {
   correctAnswer: 1,
 };
 
-function start(params) {
-  const elements = document.querySelectorAll(".alternative");
-  elements.forEach((el, i) => {
-    el.textContent = question.alternatives[i];
+let app = {
+  start: function () {
+    const elements = document.querySelectorAll(".alternative");
+    elements.forEach((el, i) => {
+      el.textContent = question.alternatives[i];
 
-    // Add handler to *all* list items
-    el.addEventListener("click", (e) => {
-      // check answer
-      console.log("check answer");
+      // Add handler to *all* list items
+      el.addEventListener("click", (e) => {
+        // check answer
+        console.log("check answer");
+      });
     });
-  });
 
-  showQuestion(question);
-}
+    this.showQuestion(question);
+  },
 
-function showQuestion(q) {
-  // 1. select dom element
-  let titleEl = document.getElementById("title");
+  showQuestion: function (q) {
+    // 1. select dom element
+    let titleEl = document.getElementById("title");
 
-  // 2. modify it
-  titleEl.textContent = question.title;
-}
+    // 2. modify it
+    titleEl.textContent = question.title;
+  },
+};
 
 // showQuestion(question);
-start();
+app.start();
 
 document
   .getElementById("btn")
